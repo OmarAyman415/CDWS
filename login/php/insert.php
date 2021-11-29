35dcf2b97da39072d6f930c08a5520676770491e
Amr Mostafa <68752132+amr-22@users.noreply.github.com>
2021-11-29 20:07:23 +0200

    Update insert.php

---
+++ login/php/insert.php
@@ -0,0 +1,19 @@
+<?php
+	$connect=mysqli_connect("localhost","root","","phpclasses") or die("Connection Faild");
+	if(!empty($_POST['save']))
+	{
+		$username=$_POST['email'];
+		$password=$_POST['password'];
+		$query="select * from login where username='$username' and password ='$password'";
+		$result=mysqli_query($connect,$query);
+		$count=mysqli_num_rows($result);
+		if($count>0)
+		{
+			echo"Login Successful";
+		}
+		else
+		{
+			echo"login not successful";
+		}
+	}
+?>
