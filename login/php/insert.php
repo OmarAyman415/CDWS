74f95badc4eafeda1e720022e2c58f18952a3e7e
OmarAyman415 <Omar@o.ayman415@gmail.com>
2021-11-29 22:20:31 +0200

    fixed insert.php file

--- login/php/insert.php
+++ login/php/insert.php
@@ -5,18 +5,16 @@
 	//}
 	//else{
 		echo 3;
-		echo !empty($_POST['submit']);
 	if( isset($_POST['submit'])){
 		$username=$_POST['email'];
 		$password=$_POST['password'];
 		$query= 'select * from login where EMAIL="'. $username .'" and PASSWORD ="'.$password .'"';
 		$result=mysqli_query($connect,$query);
-		echo $result;
 		$count = mysqli_num_rows($result);
-		
+		echo $count;
 		if($count)
 		{
-			echo"Login Successful";
+			header('Location:../../Admin/approval/index.html');
 		}
 		else
 		{
