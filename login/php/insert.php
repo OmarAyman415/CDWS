20595e39f718908a2f5da51bdc33f37529f6b61e
OmarAyman415 <Omar@o.ayman415@gmail.com>
2021-11-29 22:31:08 +0200

    removed scroll bar in tables

--- login/php/insert.php
+++ login/php/insert.php
@@ -1,17 +1,11 @@
 <?php
 	$connect = mysqli_connect("localhost","root","","cdws") or die("Connection Failed");
-	//if($connect->connect_error){
-		//die('connection Failed : '. $connect->connect_error);
-	//}
-	//else{
-		echo 3;
 	if( isset($_POST['submit'])){
 		$username=$_POST['email'];
 		$password=$_POST['password'];
 		$query= 'select * from login where EMAIL="'. $username .'" and PASSWORD ="'.$password .'"';
 		$result=mysqli_query($connect,$query);
 		$count = mysqli_num_rows($result);
-		echo $count;
 		if($count)
 		{
 			header('Location:../../Admin/approval/index.html');
@@ -21,5 +15,4 @@
 			echo"login not successful";
 		}
 	}
-	//}
 ?>
