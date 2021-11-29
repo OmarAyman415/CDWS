b54f37c61fe8c718a9e41d40f407c6232d68c36b
OmarAyman415 <Omar@o.ayman415@gmail.com>
2021-11-29 22:12:14 +0200

    updated insert

--- login/php/insert.php
+++ login/php/insert.php
@@ -1,16 +1,20 @@
 <?php
-	$connect = mysqli_connect("localhost","root","","cdws") or die("Connection Faild");
+	$connect = mysqli_connect("localhost","root","","cdws") or die("Connection Failed");
 	//if($connect->connect_error){
 		//die('connection Failed : '. $connect->connect_error);
 	//}
 	//else{
-	if(isset($_POST['submit'])){
+		echo 3;
+		echo !empty($_POST['submit']);
+	if( isset($_POST['submit'])){
 		$username=$_POST['email'];
 		$password=$_POST['password'];
-		$query="select * from login where username='$username' and password ='$password'";
+		$query= 'select * from login where EMAIL="'. $username .'" and PASSWORD ="'.$password .'"';
 		$result=mysqli_query($connect,$query);
-		$count=mysqli_num_rows($result);
-		if($count>0)
+		echo $result;
+		$count = mysqli_num_rows($result);
+		
+		if($count)
 		{
 			echo"Login Successful";
 		}
