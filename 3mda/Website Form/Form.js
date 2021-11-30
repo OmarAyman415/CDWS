31c62509540e2ddf7af74278388faba88ee18475
OmarAyman415 <Omar@o.ayman415@gmail.com>
2021-11-30 19:34:35 +0200

    added 3mda's project

---
+++ 3mda/Website Form/Form.js
@@ -0,0 +1,11 @@
+var radios = document.getElementsByName("rad2");
+var form =  document.getElementById("Hidden");
+
+document.getElementById("Hidden").style.display = 'block';
+
+for(var i = 0; i < radios.length; i++) {
+    radios[i].onclick = function() {
+      var val = this.id;
+      
+    }
+}
