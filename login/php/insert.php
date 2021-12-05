057303a715c837fb6bbe1cdc75dabfd5a05a347e
OmarAyman415 <Omar@o.ayman415@gmail.com>
2021-12-05 16:59:30 +0200

    updated the theme

--- login/php/insert.php
+++ login/php/insert.php
@@ -3,16 +3,23 @@
 	if( isset($_POST['submit'])){
 		$username=$_POST['email'];
 		$password=$_POST['password'];
+		//query that request all login records that matches that entered login data
 		$query= 'select * from login where EMAIL="'. $username .'" and PASSWORD ="'.$password .'"';
+
+		//Excute the query
 		$result=mysqli_query($connect,$query);
+
+		//record fetched from the database
 		$count = mysqli_num_rows($result);
-		if($count)
-		{
+		
+		//if Username and password are correct then go to approval page
+		if($count){
 			header('Location:../../bq/approval/ApprovalPage.php');
 		}
-		else
-		{
-			echo"login not successful";
+		//else show message that they are incorrect
+		else{
+			echo '<script>alert("Wrong Username or password")</script>';
+			header('Location:../index.html');
 		}
 	}
 ?>
